20e78a1ac38438b2a8294601dc6b041b79c4a20e
rachel4w2 <rachel4w2@naver.com>
2023-09-18 14:12:09 +0900

    design : Header 완성

---
+++ week1/resume-clone/src/components/Header.js
@@ -0,0 +1,70 @@
+function Header() {
+  return (
+    <div className="flex flex-col">
+      <div className="w-screen h-[12px] bg-pink-100 mb-[20px]"></div>
+      <div className="flex justify-center">
+        <img
+          className="w-28 h-[137px]"
+          src="https://github.com/Yujin-Baek/highcharts/assets/87238236/99fa56e7-45dc-432e-8784-ff804d6bccf3"
+          alt="avatar"
+        />
+        <div className="flex-col ml-12">
+          <h1 className="text-[37px] font-semibold">백유진</h1>
+          <h2 className="text-[28px] text-pink-200 font-medium">
+            Frontend Engineer
+          </h2>
+        </div>
+        <table className="text-sm text-left ml-44">
+          <tbody>
+            <tr className="bg-white border-b border-gray-300">
+              <th scope="row" className="py-1 font-medium">
+                Phone
+              </th>
+              <td className="pl-8 py-1">010-1234-2222</td>
+            </tr>
+            <tr className="bg-white border-b border-gray-300">
+              <th scope="row" className="py-1 font-medium">
+                Email
+              </th>
+              <td className="pl-8 py-1 underline text-gray-500">
+                yujin.px@gmail.com
+              </td>
+            </tr>
+            <tr className="bg-white border-b border-gray-300">
+              <th scope="row" className="py-1 font-medium">
+                GitHub
+              </th>
+              <td className="pl-8 py-1 underline text-gray-500">
+                <a href="https://github.com/Yujin-Baek">
+                  https://github.com/Yujin-Baek
+                </a>
+              </td>
+            </tr>
+            <tr className="bg-white border-b border-gray-300">
+              <th scope="row" className="py-1 font-medium">
+                Medium
+              </th>
+              <td className="pl-8 py-1 underline text-gray-500">
+                <a href="https://medium.com/@yujin.px">
+                  https://medium.com/@yujin.px
+                </a>
+              </td>
+            </tr>
+            <tr className="bg-white border-b border-gray-300">
+              <th scope="row" className="py-1 font-medium">
+                Tistory
+              </th>
+              <td className="pl-8 py-1 underline text-gray-500">
+                <a href="https://sweetraincodingnote.tistory.com/">
+                  https://sweetraincodingnote.tistory.com/
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ week1/resume-clone/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      pink: {
+        100: "#ffc6c6",
+        200: "#f45988",
+      },
+      gray: {
+        100: "#f3f4f6",
+        200: "#e5e7eb",
+        300: "#d1d5db",
+        400: "#9ca3af",
+        500: "#6b7280",
+        600: "#4b5563",
+        700: "#374151",
+        800: "#1f2937",
+      },
+    },
+  },
+  plugins: [],
+};
